bff7a0bb635416c0d4a34b9bd77947b3dbc52873
Karamala Ahmed <ahmed.samkhan@gmail.com>
2016-02-25 13:42:59 -0800

    Initialize repo

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var middleware ={
+
+	requireAuthentication: function(req,res,next){
+		console.log('private route hit');
+		next();
+	},
+
+	logger:function(req,res,next){
+		console.log("Request: "+req.method +'  '+ new Date().toString()+ ' ' +req.originalUrl);
+		next();
+
+	}
+}
+
+app.use(middleware.logger);
+
+app.get('/',function(req,res){
+res.send('Hello Express!');
+
+});
+
+//app.use(middleware.requireAuthentication);
+
+app.get('/about',middleware.requireAuthentication,function(req,res){
+res.send('About Us');
+});
+
+
+app.use(express.static(__dirname + '/public'));
+app.listen(port, function(){
+	console.log('Server Started :'+port);
+});
+
+
